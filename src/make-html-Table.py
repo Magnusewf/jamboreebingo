9402a2a48428dd45646c8e3c6a87ea7740768f27
haraldwf <harald.weedon-fekjar@medisin.uio.no>
2023-07-19 22:11:14 +0200

    Addes questions from file

--- src/make-html-Table.py
+++ src/make-html-Table.py
@@ -1,10 +1,16 @@
-#### Make basic bingo table i html format ####
+#### Read "bingo_tasks.txt" into a list ####
+f = open("bingo_tasks.txt", "r")
+tasks = f.read().split("\n")
+f.close()
 
+#### Make basic bingo table i html format ####
 f = open("build/bingo-table.html", "w")
 
 # Add html header to the file:
 f.write("""
 <!doctype html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />        
 <html>
 <head><title>Jamboree Bingo</title></head>
 <body>
@@ -49,7 +55,7 @@ for x in range(5):
       f.write(f"<td id=\"{x},{y}\" ")
       f.write("onclick=\"QuestionOK(this);\"")
       f.write(">")
-      f.write("test")
+      f.write(tasks[x*5+y])
       f.write("</td>\n")
     f.write("</tr>\n")
 f.write("</table>\n")

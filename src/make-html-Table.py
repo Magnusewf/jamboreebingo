70234a2721a290abb51ff7ce0f084c72e8af61a7
haraldwf <harald.weedon-fekjar@medisin.uio.no>
2023-07-24 10:50:36 +0200

    Added google analytics

--- src/make-html-Table.py
+++ src/make-html-Table.py
@@ -145,6 +145,18 @@ function loadCompletedTasks(){
 </script>               
 """)
 
+# Add google analytics to the file:
+f.write("""        
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0DFLEGP48P"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0DFLEGP48P');
+</script>
+""")
 
 f.write("""        
 </head>
